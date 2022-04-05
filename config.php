3a3948e9174eba905d8eaf9db7d932088ca32240
luccui000 <luccui2k@gmail.com>
2022-04-05 23:23:57 +0700

    [Sua]: cap nhat lai config

--- config.php
+++ config.php
@@ -2,17 +2,17 @@
 
 // Config for database
 if(!defined('HOSTNAME'))
-    define('HOSTNAME', 'localhost');
+    define('HOSTNAME', 'sql6.freemysqlhosting.net');
 if(!defined('USERNAME'))
-    define('USERNAME', 'root');
+    define('USERNAME', 'sql6483851');
 if(!defined('PASSWORD'))
-    define('PASSWORD', 'Pass@123');
+    define('PASSWORD', 'xUHqXNEgld');
 if(!defined('DB_NAME'))
-    define('DB_NAME', 'dulichtravinh');
+    define('DB_NAME', 'sql6483851');
 
 // config base app
 if(!defined('base_url'))
-    define('BASE_URL','http://dulichtravinh.local/');
+    define('BASE_URL','http://dulichtravinh.com/');
 if(!defined('base_app'))
     define('BASE_APP', str_replace('\\','/',__DIR__).'/' );
 
