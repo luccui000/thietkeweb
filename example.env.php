14428ae6117f2afb6fbefe4f53c6ab4eb7e7b9c6
luccui000 <luccui2k@gmail.com>
2022-04-05 22:16:37 +0700

    [Them]: them mau vi du ve file env.php

---
+++ example.env.php
@@ -0,0 +1,9 @@
+<?php
+
+// define login with google
+if (!defined('GOOGLE_APP_ID'))
+    define('GOOGLE_APP_ID', '');
+if (!defined('GOOGLE_APP_SECRET'))
+    define('GOOGLE_APP_SECRET', '');
+if (!defined('GOOGLE_APP_CALLBACK_URL'))
+    define('GOOGLE_APP_CALLBACK_URL', '');
